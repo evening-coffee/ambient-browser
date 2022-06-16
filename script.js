84bf7ae45222a3c8663719666356735aa299fd52
rogerkorp <99516686+evening-coffee@users.noreply.github.com>
2022-06-15 21:09:18 -0400

    :sparkles: Got Class Switching Working
    Created a sample JavaScript function where the class of the HTML tag is changed depending on the users choice in the menu.

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+
+
+
+const htmlClass = document.getElementById('root');
+
 function hamburgerSlideOver(){
-    const hamburgerMenu = document.getElementById('menu')
+    const hamburgerMenu = document.getElementById('menu');
     if (hamburgerMenu.style.display === 'none'){
         hamburgerMenu.style.display = 'flex';
     } else {
         hamburgerMenu.style.display = 'none';
+    }
+}
+
+
+
+function changeClass(change){
+    
+    if (change === beachloop){
+        let change = 'beachloop';
+        htmlClass.className = "beachloop";
+        console.log('HTML Class Name was changed to ' + change);
     }
 }
